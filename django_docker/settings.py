6014abae1d779468a5597d456053e37a17cd032d
luisgs7 <luisgsilva270@gmail.com>
2022-03-14 14:29:37 -0300

    database config

--- django_docker/settings.py
+++ django_docker/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'django_docker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'app_dj',
+        'USER': 'db_ser',
+        'PASSWORD': '1234',
+        'HOST': '172.17.0.2',
+        'PORT': '5432',
     }
 }
 
